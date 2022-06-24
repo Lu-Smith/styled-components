8ebe0df85faa9aef65cad5febfe1d4942d88a13e
Luna Smith <artistlunasmith@gmail.com>
2022-06-24 15:56:13 +0100

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    header: `#ebfbff`,
+    body: `#fff`,
+    footer: `#003333`,
+  },
+};
 
 function App() {
   return (
-    <Container>
-      <h1>Hello</h1>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container>
+          <h1>Hello</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
