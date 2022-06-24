0b2b555f5f5174bbd1f26493a55daea92918a1fa
Luna Smith <artistlunasmith@gmail.com>
2022-06-24 15:30:56 +0100

    Initial folders

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Container } from "./components/styles/Container.styled";
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+  );
+}
+
+export default App;
