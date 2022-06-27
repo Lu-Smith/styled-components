574cb13e416ea27ce9ebc2b6ab320eb94547b562
Luna Smith <artistlunasmith@gmail.com>
2022-06-27 14:19:32 +0100

    Added Card.js and content.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Container } from "./components/styles/Container.styled";
 import Header from "./components/Header";
+import Card from "./components/Card";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
 const theme = {
   colors: {
@@ -20,7 +22,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
       </>
     </ThemeProvider>
