859c2241017895dfbb749b872f67f7dc43a825c3
Luna Smith <artistlunasmith@gmail.com>
2022-06-27 13:47:30 +0100

    Updated Header.js
    Added photos and logos
    Added Flex.styled.js
    Added Button.styled.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { StyledHeader } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Flex } from "./styles/Flex.styled";
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
+import { Button } from "./styles/Button.styled";
 
 export default function Header() {
   return (
     <StyledHeader>
-      <h1>Bubble</h1>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.png" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Bubble re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+            <Button bg="#ff0099" color="#fff">
+              Get Started For Free
+            </Button>
+          </div>
+          <Image src="./images/illustration-mockups.svg" alt="random image" />
+        </Flex>
+      </Container>
     </StyledHeader>
   );
 }
